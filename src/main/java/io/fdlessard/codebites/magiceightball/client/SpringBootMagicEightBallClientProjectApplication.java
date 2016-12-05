eb01ec8aebe042db5259147ecf1c1ec934fbb8cf
fdlessard <fdlessard@gmail.com>
2016-12-04 20:41:18 -0500

    Added directory structure

---
+++ src/main/java/io/fdlessard/codebites/magiceightball/client/SpringBootMagicEightBallClientProjectApplication.java
@@ -0,0 +1,12 @@
+package io.fdlessard.codebites.magiceightball.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMagicEightBallClientProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMagicEightBallClientProjectApplication.class, args);
+	}
+}
